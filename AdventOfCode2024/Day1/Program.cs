f96a12a18c42b2ea928e30302b94f1d11d8fa0f1
Adam Moore <awmoore95@gmail.com>
2024-12-03 19:50:42 -0600

    Moved logic to Part1.cs

--- AdventOfCode2024/Day1/Program.cs
+++ AdventOfCode2024/Day1/Program.cs
@@ -9,37 +9,8 @@ namespace Day1
     {
         static void Main(string[] args)
         {
-            // Read input from file
-            var lines = File.ReadAllLines(@"Resources\Input.txt");
-
-            // Initialize lists for left and right numbers
-            List<int> leftList = new List<int>();
-            List<int> rightList = new List<int>();
-
-            // Parse the input into two lists
-            foreach (var line in lines)
-            {
-                var parts = line.Split(new[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
-                if (parts.Length >= 2)
-                {
-                    leftList.Add(int.Parse(parts[0]));
-                    rightList.Add(int.Parse(parts[1]));
-                }
-            }
-
-            // Sort both lists
-            leftList.Sort();
-            rightList.Sort();
-
-            // Compute the total distance
-            int totalDistance = 0;
-            for (int i = 0; i < leftList.Count; i++)
-            {
-                totalDistance += Math.Abs(leftList[i] - rightList[i]);
-            }
-
-            // Output the total distance
-            Console.WriteLine($"Total distance: {totalDistance}");
+            Part1 part1 = new Part1();
+            part1.Run();
         }
     }
 }
